f34db5bda9aeb040fa902451f718aad65e3c6811
Lovish Chopra <lovishchopra98@gmail.com>
2024-05-06 17:58:16 -0700

    Kademlia tree done

--- consistent_hash/consistent_hash_kademlia.go
+++ consistent_hash/consistent_hash_kademlia.go
@@ -4,6 +4,8 @@ import (
 	"crypto/sha256"
 	"encoding/binary"
 	"fmt"
+	"math/rand/v2"
+	"strconv"
 )
 
 type TrieNode struct {
@@ -20,8 +22,17 @@ func newNode() *TrieNode {
 	return &TrieNode{}
 }
 
-func NewTrie() *Trie {
-	return &Trie{root: newNode()}
+func NewTrie(ipAddresses []string, replicaPerNode int) *Trie {
+	trie := &Trie{root: newNode()}
+
+	// Add IP addresses to the hash table
+	for _, ip := range ipAddresses {
+		for replica_number := 0; replica_number < replicaPerNode; replica_number++ {
+			trie.insert(ip, replica_number)
+			fmt.Printf("Inserted IP %v, replica number %v\n", ip, replica_number)
+		}
+	}
+	return trie
 }
 
 func getTrieKey(key string) uint32 {
@@ -33,28 +44,27 @@ func getTrieKey(key string) uint32 {
 	return trie_key
 }
 
-func (t *Trie) insert(key string) {
-	trie_key := getTrieKey(key)
+func (t *Trie) insert(ip_address string, replica_number int) {
+	trie_key := getTrieKey(ip_address + strconv.Itoa(replica_number))
 	node := t.root
-	for i := 0; i < 32; i++ {
-		index := trie_key & 1
+	for i := 31; i >= 0; i-- {
+		index := (trie_key >> i) & 1
 		if node.children[index] == nil {
 			node.children[index] = newNode()
 		}
 		node = node.children[index]
-		trie_key = (trie_key >> 1)
 	}
 	node.isServer = true
-	node.ipAddress = key
+	node.ipAddress = ip_address
 }
 
 func (t *Trie) search(key string) string {
 	trie_key := getTrieKey(key)
 	node := t.root
-	for i := 0; i < 32; i++ {
-		index := trie_key & 1
+	for i := 31; i >= 0; i-- {
+		index := (trie_key >> i) & 1
 		if node.children[index] == nil {
-			node = node.children[^index]
+			node = node.children[1-index]
 		} else {
 			node = node.children[index]
 		}
@@ -63,12 +73,24 @@ func (t *Trie) search(key string) string {
 }
 
 func main() {
-	trie := NewTrie()
-	// Insert binary numbers
-	trie.insert("1010")
-	trie.insert("110")
-	trie.insert("1001")
-	// Search for binary numbers
-	fmt.Println(trie.search("1010")) // true
-	fmt.Println(trie.search("1101")) // false
+	fmt.Println("*** Kademlia Tree Initialization ***")
+	nodeAddresses := []string{"8.8.8.8", "1.1.1.1", "208.67.222.222", "208.67.220.220", "9.9.9.9"}
+	replicaPerNode := 10
+	trie := NewTrie(nodeAddresses, replicaPerNode)
+
+	fmt.Println("\n*** Sanity Check ***")
+	// a map to count the number of url valuwa per IP address
+	ipAddressCount := make(map[string]int)
+
+	for i := 0; i < replicaPerNode*1000; i++ {
+		url := fmt.Sprintf("www.%v.com", rand.IntN(100000))
+		ip := trie.search(url)
+		ipAddressCount[ip]++
+	}
+
+	// print the number of url values per IP address
+	for ip, count := range ipAddressCount {
+		fmt.Printf("IP: %v, Count: %v\n", ip, count)
+	}
+	fmt.Println("Ideal Count Per Node: ", replicaPerNode*1000/len(nodeAddresses))
 }
