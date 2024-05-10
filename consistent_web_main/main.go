0620c79d91ab5871a090e4a2091409cfac270aa7
Lovish Chopra <lovishchopra98@gmail.com>
2024-05-09 17:36:47 -0700

    Update send and receive heartbeats from master

--- consistent_web_main/main.go
+++ consistent_web_main/main.go
@@ -7,41 +7,82 @@ import (
 	"web_main/consistent_hash"
 )
 
-func main() {
-	// TODO: Update this with local web cache port numbers
-	nodeAddresses := []string{"56085", "56086", "56087", "56088", "56089"}
-	replicaPerNode := 10
-	consistentHash := consistent_hash.NewTrie(nodeAddresses, replicaPerNode)
+type Master struct {
+	masterPort     int
+	nodeAddresses  []string
+	replicaPerNode int
+}
+
+func (master Master) processPostRequest(w http.ResponseWriter, r *http.Request) {
+	// TODO: change this later based upon IP address
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Error parsing form", http.StatusBadRequest)
+		return
+	}
+
+	// Get the port from the form data
+	port := r.Form.Get("port")
+	if port == "" {
+		http.Error(w, "Port parameter is missing", http.StatusBadRequest)
+		return
+	}
+
+	// Process the heartbeat (for example, you can log it)
+	fmt.Printf("Received heartbeat from port %s\n", port)
+
+	// Respond with a success message
+	w.WriteHeader(http.StatusOK)
+}
+
+func (master Master) serve() {
+	consistentHash := consistent_hash.NewTrie(master.nodeAddresses, master.replicaPerNode)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		url := r.URL.Query().Get("url")
-		if url == "" {
-			http.Error(w, "Missing 'url' query parameter", http.StatusBadRequest)
-			return
+		if r.Method == http.MethodPost {
+			master.processPostRequest(w, r)
 		}
+		if r.Method == http.MethodGet {
+			url := r.URL.Query().Get("url")
+			if url == "" {
+				http.Error(w, "Missing 'url' query parameter", http.StatusBadRequest)
+				return
+			}
 
-		// Find the IP address of the node that will serve the URL
-		ip := consistentHash.Search(url)
-		// Fetch content from the web
-		resp, err := http.Get(fmt.Sprintf("http://localhost:%v//%v?url=%v", ip, ip, url))
+			// Find the IP address of the node that will serve the URL
+			ip := consistentHash.Search(url)
+			// Fetch content from the web
+			resp, err := http.Get(fmt.Sprintf("http://localhost:%v//%v?url=%v", ip, ip, url))
 
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error fetching URL: %v", err), http.StatusInternalServerError)
-			return
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Error fetching URL: %v", err), http.StatusInternalServerError)
+				return
+			}
+			defer resp.Body.Close()
+
+			content, err := io.ReadAll(resp.Body)
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Error reading response body: %v", err), http.StatusInternalServerError)
+				return
+			}
+
+			// Serve fetched content
+			w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+			w.Write(content)
 		}
-		defer resp.Body.Close()
-
-		content, err := io.ReadAll(resp.Body)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error reading response body: %v", err), http.StatusInternalServerError)
-			return
-		}
-
-		// Serve fetched content
-		w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
-		w.Write(content)
 	})
 
-	fmt.Println("Server started on :5050")
-	http.ListenAndServe(":5050", nil)
+	serveAddr := fmt.Sprintf(":%d", master.masterPort)
+	fmt.Println("Server started on ", serveAddr)
+	http.ListenAndServe(serveAddr, nil)
+}
+
+func main() {
+	// TODO: Update this with local web cache port numbers
+	master := Master{
+		masterPort:     5050,
+		nodeAddresses:  []string{"58535", "58536", "58537", "58538", "58539"},
+		replicaPerNode: 10,
+	}
+	master.serve()
 }
--- heartbeats/heartbeat.go
+++ heartbeats/heartbeat.go
@@ -2,35 +2,39 @@ package main
 
 import (
 	"fmt"
-	"net"
+	"net/http"
+	"net/url"
 	"time"
 )
 
-func sendHeartbeat(conn net.Conn) {
+func sendHeartbeat(masterAddr string, port string) {
 	for {
-		_, err := conn.Write([]byte("heartbeat"))
+		// Construct the URL for the heartbeat endpoint
+		endpoint := fmt.Sprintf("http://%s/heartbeat", masterAddr)
+
+		// Construct the POST data
+		postData := url.Values{}
+		postData.Set("port", port)
+
+		// Send heartbeat POST request to master
+		_, err := http.PostForm(endpoint, postData)
 		if err != nil {
 			fmt.Println("Error sending heartbeat:", err)
-			return
+		} else {
+			fmt.Println("Sent heartbeat to master from port:", port)
 		}
+
 		time.Sleep(5 * time.Second) // Send heartbeat every 5 seconds
 	}
 }
 
 func main() {
 	// Master address
-	masterAddr := "localhost:8080"
+	masterAddr := "localhost:5050"
 
-	// Connect to master
-	conn, err := net.Dial("tcp", masterAddr)
-	if err != nil {
-		fmt.Println("Error connecting to master:", err)
-		return
-	}
-	defer conn.Close()
-
-	fmt.Println("Connected to master at", masterAddr)
+	// Port of this server. TODO change this later
+	port := "12345"
 
 	// Start sending heartbeats
-	sendHeartbeat(conn)
+	sendHeartbeat(masterAddr, port)
 }
