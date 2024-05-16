84d1e30097e255179634a8bf8faa64ee8cd43b9f
neha <neha.kunjal@gmail.com>
2024-05-15 19:17:21 -0700

    adding base code of keeping track of hotkeys via a in memory map using moving average technique

--- consistent_web_main/main.go
+++ consistent_web_main/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"io"
+	"math"
+	"math/rand"
 	"net/http"
+	"sync"
 	"time"
 	"web_main/consistent_hash"
 )
@@ -13,6 +16,37 @@ type Main struct {
 	nodeAddresses    []string
 	replicaPerNode   int
 	nodeTimestampMap map[string]time.Time
+}
+
+type HotKeyEntry struct {
+	Average          float64
+	PastTimeRequest  int64
+}
+
+type HotKeys struct {
+	KeyMap        map[string] HotKeyEntry
+	mutex   sync.RWMutex
+}
+
+func Keys() *HotKeys {
+	return &HotKeys{
+		KeyMap: make(map[string] HotKeyEntry),
+	}
+}
+
+// Get retrieves a cached entry by key
+func (c *HotKeys) Get(url string) (HotKeyEntry, bool) {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+	entry, ok := c.KeyMap[url]
+	return entry, ok
+}
+
+// Set inserts or updates a cached entry
+func (hk *HotKeys) Set(url string, entry HotKeyEntry) {
+	hk.mutex.Lock()
+	defer hk.mutex.Unlock()
+	hk.KeyMap[url] = entry
 }
 
 func (main Main) processPostRequest(w http.ResponseWriter, r *http.Request) {
@@ -41,6 +75,12 @@ func (main Main) processPostRequest(w http.ResponseWriter, r *http.Request) {
 func (main Main) serve() {
 	consistentHash := consistent_hash.NewTrie(main.nodeAddresses, main.replicaPerNode)
 
+	// TODO figure out best threshold / k value
+	threshhold := 3.0
+	k := 0.5
+
+	hotUrls := Keys()
+
 	// Start the heartbeat server
 	http.Handle("/heartbeat", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		main.processPostRequest(w, r)
@@ -48,15 +88,50 @@ func (main Main) serve() {
 
 	// Start the main server
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		now := time.Now().Unix()
+
 		url := r.URL.Query().Get("url")
 		if url == "" {
 			http.Error(w, "Missing 'url' query parameter", http.StatusBadRequest)
 			return
 		}
 
-		// Find the IP address of the node that will serve the URL
-		ip := consistentHash.Search(url)
+		ip := ""
+		// Find the IP address of the node that will serve the URL if url is hot
+		value, exists := hotUrls.Get(url)
+		if exists  {
+			if value.Average >= threshhold {
+				fmt.Println("Threshold reached, randomly dispersing.")
+				ip = main.nodeAddresses[rand.Intn(len(main.nodeAddresses))]
+			} else {
+				ip = consistentHash.Search(url)
+			}
 
+			if value.PastTimeRequest == now {
+				fmt.Println("Same request in current second, calculating moving average.")
+				hotUrls.Set(url, HotKeyEntry{
+					Average: value.Average + 1,
+					PastTimeRequest:  value.PastTimeRequest,
+				})
+			} else {
+				fmt.Println("Moving average update for time of different second.")
+
+				seconds := (float64)(now - value.PastTimeRequest)
+				newAverage := value.Average * math.Pow(k, seconds) + 1
+				hotUrls.Set(url, HotKeyEntry{
+					Average: newAverage,
+					PastTimeRequest:  now,
+				})
+			}
+		} else {
+			fmt.Println("Starting entry of moving average.")
+			ip = consistentHash.Search(url)
+			hotUrls.Set(url, HotKeyEntry{
+				Average: 1,
+				PastTimeRequest: now,
+			})
+		}
+		
 		for time.Now().Sub(main.nodeTimestampMap[ip]) > 15*time.Second {
 			consistentHash.DeleteNode(ip)
 			ip = consistentHash.Search(url)
@@ -89,7 +164,7 @@ func (main Main) serve() {
 
 func main() {
 	nodeTimestampMap := make(map[string]time.Time)
-	nodeAddresses := []string{"34.125.246.49", "34.125.32.120"}
+	nodeAddresses := []string{"localhost", "10.30.147.20"}
 
 	// Initialize for time.Now() + 60 seconds to allow for starting
 	// everything up
