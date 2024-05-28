829a4f8bd464563b6c0f23df3b8488cab84b56b4
macvincent <macvincentaghaoko@gmail.com>
2024-05-27 17:24:42 -0700

    revert cache change

--- web_cache/main.go
+++ web_cache/main.go
@@ -123,8 +123,5 @@ func main() {
 	})
 
 	log.Println("Server started on :8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatalf("Failed to start HTTP server: %v", err)
-	}
+	http.ListenAndServe(":8080", nil)
 }
