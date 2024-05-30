59f60875c5e642297db2e577286c95aead78ffda
macvincent <macvincentaghaoko@gmail.com>
2024-05-29 23:02:35 -0700

    update shell commands

--- local_setup.sh
+++ local_setup.sh
@@ -1,6 +1,6 @@
 #update this variable with your username
 username=macvincent
-instance_name=go-vm2
+instance_name=go-vm3
 
-gcloud compute scp --recurse --compress ./web_cache $instance_name:/home/$username
-gcloud compute ssh $instance_name --command "cd web_cache && go mod tidy && go run main.go" 
+gcloud compute scp --recurse --compress ./cache_startup.sh ./web_cache $instance_name:/home/$username
+gcloud compute ssh $instance_name --command "chmod +x ./cache_startup.sh && ./cache_startup.sh" 
