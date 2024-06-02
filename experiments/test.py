7ae89dc3ca16ac7dbafd84b3fcf0159bddc15b50
macvincent <macvincentaghaoko@gmail.com>
2024-06-01 19:51:05 -0700

    latest

--- experiments/test.py
+++ experiments/test.py
@@ -2,16 +2,18 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 # Load csv data from file as numpy array
-for replica_count in [1, 10, 100, 1000, 10000, 100000, 1000000]:
-    file_name = f'./kademlia_vs_cycle/kademlia/latency_{replica_count}_replicas.csv'
-    data = np.loadtxt(file_name, delimiter=',')
-    plt.plot(np.log(replica_count), np.percentile(data, 99), 'ro')
+plt.plot(0, 0, 'bo')
+# for replica_count in [1, 10, 100, 1000, 10000, 100000, 1000000]:
+#     file_name = f'./kademlia_vs_cycle/kademlia/1-{replica_count}.txt'
+#     data = np.loadtxt(file_name, delimiter=',')
+#     plt.plot(np.log(replica_count), np.mean(data), 'ro')
     
+# plt.plot(0, 0, 'bo')
 for replica_count in [1, 10, 100, 1000, 10000, 100000, 1000000]:
     file_name = f'./kademlia_vs_cycle/cycle/latency_{replica_count}_replicas.csv'
     data = np.loadtxt(file_name, delimiter=',')
     # blue color
-    plt.plot(np.log(replica_count), np.percentile(data, 99), 'bo')
+    plt.plot(np.log(replica_count), np.mean(data), 'bo')
 
 plt.show()
 
